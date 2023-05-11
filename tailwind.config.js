e9be7651bf56b392590338e248dc59593362588b
SamColt36 <samuelss.contato@gmail.com>
2023-05-11 15:21:54 -0300

    Reformulado container  bannner para desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "dark",
-  content: ["*.{html,js}"],
+  content: [],
   theme: {
     extend: {
       screens: {
         xl: { min: "1024px" },
-        // => @media and (min-width: 1024px) and (max-width: 1279px) { ... }
+        // => @media and (min-width: 1024px) and (max-width: 1279px)
 
         lg: { min: "768px", max: "1023px" },
-        // => @media and (min-width: 768px) and (max-width: 1023px) { ... }
+        // => @media and (min-width: 768px) and (max-width: 1023px)
 
         md: { min: "426px", max: "767px" },
-        // => @media and (min-width: 426px) and (max-width: 767px) { ... }
+        // => @media and (min-width: 426px) and (max-width: 767px)
 
         sm: { min: "320px", max: "425px" },
-        // => @media and (min-width: 320px) and (max-width: 425px) { ... }
+        // => @media and (min-width: 320px) and (max-width: 425px)
       },
       colors: {
         azul: "#89E8EC",
         laranja: "#E9473C",
       },
-      plugins: [],
     },
   },
+  plugins: [],
 };
-
